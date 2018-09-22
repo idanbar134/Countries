43a71fd7de2ba00a22bb958185a9aa3b90bb3213
luci <idan@smileback.co>
2018-09-23 00:08:47 +0300

    progress

--- app/src/main/java/com/crazyworld/countries/view/countries/CountriesFragment.kt
+++ app/src/main/java/com/crazyworld/countries/view/countries/CountriesFragment.kt
@@ -2,12 +2,15 @@ package com.crazyworld.countries.view.countries
 
 import android.arch.lifecycle.Observer
 import android.os.Bundle
+import android.support.v4.util.SparseArrayCompat
 import android.support.v7.widget.StaggeredGridLayoutManager
 import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.navigation.fragment.findNavController
 import com.crazyworld.countries.R
 import com.crazyworld.countries.common.BaseFragment
 import com.crazyworld.countries.common.Constants.BUNDLE_COUNTRY
+import com.crazyworld.countries.common.ViewTypeDelegateAdapter
 import kotlinx.android.synthetic.main.countries_fragment.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -21,18 +24,11 @@ class CountriesFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        adapter.delegateAdapters.apply {
-            put(CountryItemType.ItemType, DelegateCountryAdapter {
-                val bundle = Bundle()
-                bundle.putParcelable(BUNDLE_COUNTRY,it)
-                findNavController().navigate(R.id.action_countries_to_details,bundle)
-            })
-        }
-        val layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
-        layoutManager.gapStrategy = StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
-        countriesList.layoutManager = layoutManager
+        adapter.delegateAdapters.apply { applyCountryType() }
+        val controller = AnimationUtils.loadLayoutAnimation(context, R.anim.fall_down)
+        countriesList.layoutAnimation = controller
         countriesList.adapter = adapter
+        countriesList.scheduleLayoutAnimation()
     }
 
     override fun render() {
@@ -42,6 +38,14 @@ class CountriesFragment : BaseFragment() {
             when (state) {
                 is DataReady -> adapter.addData(state.data)
             }
+        })
+    }
+
+    private fun SparseArrayCompat<ViewTypeDelegateAdapter>.applyCountryType() {
+        put(CountryItemType.ItemType, DelegateCountryAdapter { country ->
+            val bundle = Bundle()
+            bundle.putParcelable(BUNDLE_COUNTRY, country)
+            findNavController().navigate(R.id.action_countries_to_details, bundle)
         })
     }
 }
--- app/src/main/java/com/crazyworld/countries/view/countries/DelegateAdapter.kt
+++ app/src/main/java/com/crazyworld/countries/view/countries/DelegateAdapter.kt
@@ -34,6 +34,8 @@ class DelegateCountryAdapter(var onClick: (CountriesRender) -> Unit) : ViewTypeD
     }
 }
 
+
+
 object CountryItemType {
     const val ItemType = 0
 }
